4b80b667aadb20c508ca7e54d223468ca03a71d0
Daniel Lopez <dlopez807@gmail.com>
2022-09-17 10:12:00 -0700

    style read link as 3d button

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,9 +30,7 @@ const Button = ({
   const chapterLink = `https://www.jw.org/finder?srcid=jwlshare&wtlocale=E&prefer=lang&bible=${bibleText}&pub=nwtsty`
   return (
     <a
-      className={`text-lg font-bold hover:text-cb-pink ${
-        readToday ? 'text-cb-light-blue' : ''
-      }`}
+      className='group cursor-pointer rounded-lg border-none bg-cb-dark-blue text-lg'
       href={chapterLink}
       target='_blank'
       rel='noopener noreferrer'
@@ -54,8 +52,14 @@ const Button = ({
         }
       }}
     >
-      read chapter: {bookAndChapter}
-      {readToday ? ' again' : ''}
+      <span
+        className={`block translate-y-[-4px] transform rounded-lg bg-[#5a3e84] px-2 py-2 text-lg duration-[600ms] ease-[cubic-bezier(.3,.7,.4,1)] hover:ease-[cubic-bezier(.3,.7,.4,1.5)] group-hover:translate-y-[-6px] group-hover:duration-[250ms] group-active:translate-y-[-2px] group-active:duration-[34ms] ${
+          readToday ? 'text-cb-yellow' : 'text-gray-100'
+        }`}
+      >
+        read chapter: {bookAndChapter}
+        {readToday ? ' again' : ''}
+      </span>
     </a>
   )
 }
