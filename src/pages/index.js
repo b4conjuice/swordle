9417a895c248c338a86bf88f2164c4be10f2bc11
Daniel Lopez <dlopez807@gmail.com>
2022-09-20 19:02:18 -0700

    update button styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ const Button = ({
   const chapterLink = `https://www.jw.org/finder?srcid=jwlshare&wtlocale=E&prefer=lang&bible=${bibleText}&pub=nwtsty`
   return (
     <a
-      className='group cursor-pointer rounded-lg border-none bg-cb-dark-blue text-lg'
+      className='group w-full cursor-pointer rounded-lg border-none bg-cb-dark-blue text-center text-lg'
       href={chapterLink}
       target='_blank'
       rel='noopener noreferrer'
@@ -58,7 +58,7 @@ const Button = ({
       }}
     >
       <span
-        className={`block translate-y-[-4px] transform rounded-lg bg-[#5a3e84] px-2 py-2 text-lg duration-[600ms] ease-[cubic-bezier(.3,.7,.4,1)] hover:ease-[cubic-bezier(.3,.7,.4,1.5)] group-hover:translate-y-[-6px] group-hover:duration-[250ms] group-active:translate-y-[-2px] group-active:duration-[34ms] ${
+        className={`block translate-y-[-4px] transform rounded-lg bg-[#5a3e84] p-3 text-lg duration-[600ms] ease-[cubic-bezier(.3,.7,.4,1)] hover:ease-[cubic-bezier(.3,.7,.4,1.5)] group-hover:translate-y-[-6px] group-hover:duration-[250ms] group-active:translate-y-[-2px] group-active:duration-[34ms] ${
           readToday ? 'text-cb-yellow' : 'text-gray-100'
         }`}
       >
@@ -92,7 +92,7 @@ const Home = () => {
   const readToday = lastRead === today
   return (
     <Page>
-      <Main className='flex flex-col items-center justify-center space-y-4'>
+      <Main className='flex flex-col items-center justify-center space-y-4 px-4'>
         <Title>swordle</Title>
         {(data || bookAndChapter) && (
           <Button
