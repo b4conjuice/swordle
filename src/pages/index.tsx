d926c77b27089e896566a9405d0abed2a9c02711
Daniel Lopez <dlopez807@gmail.com>
2024-04-26 15:36:05 -0700

    fix bug where modal would stay opned on blur

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import Title from '@/components/title'
 import books, { bookIndex, booksAndChaptersMap } from '@/utils/books'
 import useLocalStorage from '@/utils/useLocalStorage'
 import { api } from '@/utils/api'
+import Modal from '@/components/modal'
 
 const DailyTextButton = ({
   scripture,
@@ -366,98 +367,65 @@ const Home = () => {
             isOpen={isOpen}
             setIsOpen={setIsOpen}
           />
-          <Transition.Root show={isSettingsDialogOpen} as={Fragment}>
-            <Dialog
-              onClose={setIsOpen}
-              className='fixed inset-0 flex flex-col justify-end overflow-y-auto p-4'
+          <Modal
+            isOpen={isSettingsDialogOpen}
+            setIsOpen={setIsSettingsDialogOpen}
+            title='settings'
+          >
+            <select
+              className='bg-cobalt w-full p-4'
+              value={buttonType}
+              onChange={e => {
+                setButtonType(e.target.value)
+              }}
             >
-              <Transition.Child
-                enter='duration-300 ease-out'
-                enterFrom='opacity-0'
-                enterTo='opacity-100'
-                leave='duration-200 ease-in'
-                leaveFrom='opacity-100'
-                leaveTo='opacity-0'
-              >
-                <div className='bg-cobalt/90 fixed inset-0' />
-              </Transition.Child>
-              <Transition.Child
-                enter='duration-300 ease-out'
-                enterFrom='opacity-0 scale-95'
-                enterTo='opacity-100 scale-100'
-                leave='duration-200 ease-in'
-                leaveFrom='opacity-100 scale-100'
-                leaveTo='opacity-0 scale-95'
-              >
-                <Dialog.Panel className='dark:bg-cb-dusty-blue relative z-10 rounded-lg p-4 dark:text-gray-100'>
-                  <button
-                    type='button'
-                    onClick={() => setIsSettingsDialogOpen(false)}
-                    className='absolute right-4 top-4'
-                  >
-                    <XMarkIcon className='h-6 w-6' />
-                  </button>
-                  <div className='space-y-3'>
-                    <Dialog.Title className='mt-4 text-center text-xl'>
-                      settings
-                    </Dialog.Title>
-                    <select
-                      className='bg-cobalt w-full p-4'
-                      value={buttonType}
-                      onChange={e => {
-                        setButtonType(e.target.value)
-                      }}
-                    >
-                      {buttonTypes.map(bType => (
-                        <option key={bType.id} value={bType.id}>
-                          {bType.text}
-                        </option>
-                      ))}
-                    </select>
-                    {buttonType === 'sequential' && (
-                      <div className='flex'>
-                        <select
-                          className='bg-cobalt w-full p-4'
-                          value={sequence?.split(':')[0]}
-                          onChange={e => {
-                            const [, chapter] = sequence.split(':')
-                            const newSequence = `${e.target.value}:${chapter}`
-                            setSequence(newSequence)
-                          }}
-                        >
-                          {books.map((book, idx) => (
-                            <option key={idx + 1} value={idx + 1}>
-                              {book}
-                            </option>
-                          ))}
-                        </select>
-                        <select
-                          className='bg-cobalt w-full p-4'
-                          value={sequence?.split(':')[1]}
-                          onChange={e => {
-                            const [bookNumber] = sequence.split(':')
-                            const newSequence = `${bookNumber}:${e.target.value}`
-                            setSequence(newSequence)
-                          }}
-                        >
-                          {Array.from(
-                            {
-                              length: sequenceChapters,
-                            },
-                            (_, i) => i + 1
-                          ).map(ch => (
-                            <option key={ch} value={ch}>
-                              {ch}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                    )}
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </Dialog>
-          </Transition.Root>
+              {buttonTypes.map(bType => (
+                <option key={bType.id} value={bType.id}>
+                  {bType.text}
+                </option>
+              ))}
+            </select>
+
+            {buttonType === 'sequential' && (
+              <div className='flex'>
+                <select
+                  className='bg-cobalt w-full p-4'
+                  value={sequence?.split(':')[0]}
+                  onChange={e => {
+                    const [, chapter] = sequence.split(':')
+                    const newSequence = `${e.target.value}:${chapter}`
+                    setSequence(newSequence)
+                  }}
+                >
+                  {books.map((book, idx) => (
+                    <option key={idx + 1} value={idx + 1}>
+                      {book}
+                    </option>
+                  ))}
+                </select>
+                <select
+                  className='bg-cobalt w-full p-4'
+                  value={sequence?.split(':')[1]}
+                  onChange={e => {
+                    const [bookNumber] = sequence.split(':')
+                    const newSequence = `${bookNumber}:${e.target.value}`
+                    setSequence(newSequence)
+                  }}
+                >
+                  {Array.from(
+                    {
+                      length: sequenceChapters,
+                    },
+                    (_, i) => i + 1
+                  ).map(ch => (
+                    <option key={ch} value={ch}>
+                      {ch}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            )}
+          </Modal>
         </div>
       </Main>
     </Page>
