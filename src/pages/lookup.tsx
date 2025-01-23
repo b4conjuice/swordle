6ee112ab73fc94b1328855199c4c2be4dc1ff33a
Daniel Lopez <d@niel.to>
2025-01-23 12:04:35 -0800

    feat: add lookup page for scriptures

---
+++ src/pages/lookup.tsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { useForm, type SubmitHandler } from 'react-hook-form'
+
+import { api } from '@/utils/api'
+import Main from '@/components/main'
+import Page from '@/components/page'
+import copyToClipboard from '@/utils/copyToClipboard'
+
+type Inputs = {
+  scripture: string
+}
+
+const isValidScripture = (scripture: string) => scripture.includes(':')
+
+export default function SwordPage() {
+  const [scripture, setScripture] = useState<string>('')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    setScripture(data.scripture)
+  }
+
+  const scripturesQuery = api.sword.scriptures.useQuery({
+    scripture,
+  })
+  const { data: scriptureData, isLoading } = scripturesQuery
+  console.log({ scriptureData })
+  const text = scriptureData
+    ? `${scriptureData.reference}\n${scriptureData.text}`
+    : ''
+  return (
+    <Page>
+      <Main className='flex flex-col p-4'>
+        <div className='flex flex-grow flex-col space-y-4'>
+          <h1>sword</h1>
+          <form className='space-y-4' onSubmit={handleSubmit(onSubmit)}>
+            <input
+              className='bg-cobalt block w-full'
+              placeholder='gen1:1'
+              {...register('scripture', {
+                validate: scripture => isValidScripture(scripture),
+              })}
+            />
+            <button
+              className='block w-full translate-y-[-4px] transform rounded-lg bg-[#5a3e84] p-3 text-lg duration-[600ms] ease-[cubic-bezier(.3,.7,.4,1)] hover:ease-[cubic-bezier(.3,.7,.4,1.5)] disabled:pointer-events-none disabled:opacity-25 group-hover:translate-y-[-6px] group-hover:duration-[250ms] group-active:translate-y-[-2px] group-active:duration-[34ms]'
+              type='submit'
+              disabled={!isDirty}
+            >
+              go
+            </button>
+          </form>
+          {scripture ? (
+            isLoading ? (
+              <p>loading</p>
+            ) : text ? (
+              <>
+                <textarea className='bg-cobalt' value={text} />
+                <button
+                  className='block w-full translate-y-[-4px] transform rounded-lg bg-[#5a3e84] p-3 text-lg duration-[600ms] ease-[cubic-bezier(.3,.7,.4,1)] hover:ease-[cubic-bezier(.3,.7,.4,1.5)] disabled:pointer-events-none disabled:opacity-25 group-hover:translate-y-[-6px] group-hover:duration-[250ms] group-active:translate-y-[-2px] group-active:duration-[34ms]'
+                  type='button'
+                  onClick={() => {
+                    copyToClipboard(text)
+                    toast.success('copied text')
+                  }}
+                >
+                  copy
+                </button>
+              </>
+            ) : (
+              <p>no results</p>
+            )
+          ) : null}
+        </div>
+      </Main>
+    </Page>
+  )
+}
--- src/server/api/routers/sword.ts
+++ src/server/api/routers/sword.ts
@@ -13,12 +13,26 @@ interface SwordData {
   text: string
 }
 
+interface SciptureData {
+  success: boolean
+  scripture: string
+  text: string
+  reference: string
+}
 export const swordRouter = createTRPCRouter({
   dt: publicProcedure
     .input(z.object({ date: z.string() }))
     .query(async ({ input }) => {
       const data: SwordData = await fetcher(
         `https://api.dlopez.app/api/sword/dt/${input.date}`
+      )
+      return data
+    }),
+  scriptures: publicProcedure
+    .input(z.object({ scripture: z.string() }))
+    .query(async ({ input }) => {
+      const data: SciptureData = await fetcher(
+        `https://api.dlopez.app/api/sword/scriptures/${input.scripture}`
       )
       return data
     }),
